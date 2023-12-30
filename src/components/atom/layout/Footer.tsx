73a0427f94aa6674c1087d31fd75240111c9d5ec
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-12-30 16:18:46 +0330

    add: layout

---
+++ src/components/atom/layout/Footer.tsx
@@ -0,0 +1,30 @@
+"use client"
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const Footer = () => {
+  return (
+    <>
+      <Box>
+      <AppBar position="static" sx={{ background: "#212529", boxShadow: "none" }}>
+        <Toolbar>
+          <Typography 
+          variant="h6"
+          component="div" 
+          sx={{ flexGrow: 1 }}
+          textAlign="center"
+          color="white"
+          >
+            &copy; MrJurgen / Aryan Salemabadi
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+        
+        </>
+  )
+}
+
+export default Footer;
